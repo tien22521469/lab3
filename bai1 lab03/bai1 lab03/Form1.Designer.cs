a9b92b76d4ff82fab8e21f275e652d7ab8d3d207
tien22521469 <163420484+tien22521469@users.noreply.github.com>
2024-04-28 11:04:04 +0700

    Add files via upload

---
+++ bai1 lab03/bai1 lab03/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace bai1_lab03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_server = new Button();
+            btn_client = new Button();
+            SuspendLayout();
+            // 
+            // btn_server
+            // 
+            btn_server.Location = new Point(101, 165);
+            btn_server.Name = "btn_server";
+            btn_server.Size = new Size(215, 95);
+            btn_server.TabIndex = 0;
+            btn_server.Text = "SERVER";
+            btn_server.UseVisualStyleBackColor = true;
+            btn_server.Click += btn_server_Click;
+            // 
+            // btn_client
+            // 
+            btn_client.Location = new Point(494, 165);
+            btn_client.Name = "btn_client";
+            btn_client.Size = new Size(215, 95);
+            btn_client.TabIndex = 1;
+            btn_client.Text = "CLIENT";
+            btn_client.UseVisualStyleBackColor = true;
+            btn_client.Click += btn_client_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_client);
+            Controls.Add(btn_server);
+            Name = "Form1";
+            Text = "UDP Chat";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btn_server;
+        private Button btn_client;
+    }
+}
---
+++ bai1 lab03/bai1 lab03/client.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace bai1_lab03
+{
+    public partial class client : Form
+    {
+        public client()
+        {
+            InitializeComponent();
+        }
+
+        private string GetLocalIPv4(NetworkInterfaceType _type)
+        {
+            string output = "";
+
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            output = ip.Address.ToString();
+            return output;
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            txt_Host.Text = GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+            if (string.IsNullOrEmpty(txt_Host.Text))
+                txt_Host.Text = GetLocalIPv4(NetworkInterfaceType.Ethernet);
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txt_Host.Text))
+            {
+                MessageBox.Show("Host không được để trống");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(txt_Port.Text))
+            {
+                MessageBox.Show("Port không được để trống");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(rtxt_Messages.Text))
+                return;
+
+            UdpClient udpClient = new UdpClient();
+            byte[] sendBytes = Encoding.UTF8.GetBytes(rtxt_Messages.Text);
+            int port = Int32.Parse(txt_Port.Text);
+            udpClient.Send(sendBytes, sendBytes.Length, txt_Host.Text, port);
+        }
+    }
+}
